cced1588dddfb8e714781bbd7990eb3210efd2f1
김회준 <rjcnd123@gmail.com>
2025-10-13 18:23:13 +0900

    fix update

--- quartz.config.ts
+++ quartz.config.ts
@@ -69,7 +69,7 @@ const config: QuartzConfig = {
         keepBackground: false,
       }),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "relative" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
