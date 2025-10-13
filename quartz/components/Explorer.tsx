f826af8b557cde5ee4df431fe06385fb6180c4e4
김회준 <rjcnd123@gmail.com>
2025-10-13 20:00:29 +0900

    updates

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -84,8 +84,8 @@ export default ((userOpts?: Partial<Options>) => {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
+            width="20"
+            height="20"
             viewBox="0 0 24 24"
             stroke-width="2"
             stroke-linecap="round"
