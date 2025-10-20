691d3748cf48b98ffda2ed2a1d2995a8eb56ec0c
김회준 <rjcnd123@gmail.com>
2025-10-20 11:01:17 +0900

    fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -59,7 +59,7 @@ const config: QuartzConfig = {
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "git", "filesystem"],
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.HardLineBreaks(),
       Plugin.SyntaxHighlighting({
